e32a80b17f36b8ef75a3875919d9232024277125
Utilisateur <Utilisateur@LAPTOP-EQ6UL4AI>
2022-10-25 16:59:09 +0200

    j'en suis a la methode decapsuler page 4

---
+++ TP_Bieres_BATARD/src/tp_bieres_batard/BouteilleBiere.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp_bieres_batard;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class BouteilleBiere {
+    
+    public String nom ;
+    public float degreAlcool ;
+    public String brasserie ; 
+    public boolean ouverte ;
+    public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+
+ 
+
+}
+public BouteilleBiere(String unNom, float unDegre, String
+uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+}
+
+
