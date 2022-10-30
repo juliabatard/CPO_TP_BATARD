1a3dfcc4cb5fad26d68289062c24a70a2a91f2cd
Utilisateur <Utilisateur@LAPTOP-EQ6UL4AI>
2022-10-30 23:37:38 +0100

    ajout de l'attribut personne proprietaire

---
+++ TP2_relation_1_BATARD/src/tp2_relation_1_batard/Voiture.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_batard;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Voiture {
+
+    public Voiture(String clio, String renault, int par) {
+    }
+public String Modele;
+public String Marque;
+public int PuissanceCV;
+public Personne proprietaire;    
+}
